9c684c9c8cb059cb6c1c6611b6565b58b4685894
gaurav-sharma-vs <sharma.gaurav@vectoscalar.com>
2024-04-15 20:13:36 +0530

    changes

--- src/clients/openAi.client.ts
+++ src/clients/openAi.client.ts
@@ -9,19 +9,19 @@ export class OpenAi {
   async invoke(prompt: string, options?: any): Promise<any> {
     const defaultOptions = {
       model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: prompt,
-        },
-      ],
       temperature: 1,
       max_tokens: 256,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
     };
-    const mergedOptions = { ...defaultOptions, ...options };
+    const message = {  messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],}
+    const mergedOptions = { ...defaultOptions, ...options, ...message };
     try {
       const response: any = await this.client.chat.completions.create(
         mergedOptions
